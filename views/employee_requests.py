a75a8a5ff263c625aa5c87286f08be68fa0dbe45
Jonathan Woodard <jvw.226@gmail.com>
2022-10-05 17:15:27 -0500

    uploaded kennel-server course work

---
+++ views/employee_requests.py
@@ -0,0 +1,25 @@
+EMPLOYEES = [
+    {
+        "id": 1,
+        "name": "Jenna Solis"
+    }
+]
+
+
+def get_all_employees():
+    return EMPLOYEES
+
+
+def get_single_employee(id):
+    # Variable to hold the found animal, if it exists
+    requested_employee = None
+
+    # Iterate the ANIMALS list above. Very similar to the
+    # for..of loops you used in JavaScript.
+    for employee in EMPLOYEES:
+        # Dictionaries in Python use [] notation to find a key
+        # instead of the dot notation that JavaScript used.
+        if employee["id"] == id:
+            requested_employee = employee
+
+    return requested_employee
